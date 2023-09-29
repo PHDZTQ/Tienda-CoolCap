b23709f8dfc7f5f958c02b659cab9fd28e62cbb5
Paola <paolaht12@gmail.com>
2023-09-29 14:41:52 -0600

    modificacion en index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+/* Abrir y cerrar el carrito de compra */
 const btnCart = document.querySelector('.container-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click', () => {
     containerCartProducts.classList.toggle('hidden-cart')
 })
+
+/* Agregar, eliminar y sumar en el carrito de compra */
+const cartInfo = document.querySelector('cart-product')
