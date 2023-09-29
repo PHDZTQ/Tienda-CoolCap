37d3aeaa8ce0b28af70c39d28e2cf0448e9e1490
Paola <paolaht12@gmail.com>
2023-09-29 15:09:06 -0600

    ultimos avances

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* Abrir y cerrar el carrito de compra */
-const btnCart = document.querySelector('.container-icon')
+const btnCart = document.querySelector('.container-cart-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click', () => {
@@ -8,3 +8,16 @@ btnCart.addEventListener('click', () => {
 
 /* Agregar, eliminar y sumar en el carrito de compra */
 const cartInfo = document.querySelector('cart-product')
+const rowProduct = document.querySelector('.row-product')
+
+//Lista de todos los contenedores de productos
+const productList = document.querySelector('.container-items')
+
+//Variable de arreglos de productos
+let allProducts = []
+
+productList.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-add-cart')){
+        
+    }
+})
